d47e9a91d2576b9dae2e5fdfeab8500fa5cbe6b9
Kyle <kj.mckinley@gmail.com>
2021-07-18 14:46:19 -0500

    added port listening and session storage in server.js

--- server.js
+++ server.js
@@ -8,3 +8,21 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 
+//add session storing
+const session = require('express-session');
+
+const storeSequelize = require('connect-session-sequelize')(session.Store);
+
+const sessionDB = {
+    secret: 'the big secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  }
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(session(sessionDB));
