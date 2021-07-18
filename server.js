6d3cc8b41e58497d7cdcfb5c280f9054ca4ab899
Kyle <kj.mckinley@gmail.com>
2021-07-18 14:59:35 -0500

    finished out server. possible changes to come.

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const hbs = exphbs.create({ helpers });
 //add session storing
 const session = require('express-session');
 
-const storeSequelize = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const sessionDB = {
-    secret: 'the big secret',
+const sess = {
+    secret: 'secret word',
     cookie: {},
     resave: false,
     saveUninitialized: true,
@@ -24,5 +24,24 @@ const sessionDB = {
   }
 
 const app = express();
+
 const PORT = process.env.PORT || 3001;
-app.use(session(sessionDB));
+
+app.use(session(sess));
+
+// give app access to use express and join path to 'public' folder
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// allow server to connect to handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
