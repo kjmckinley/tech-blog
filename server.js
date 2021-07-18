bf1a88be925735d81fe776357a1641d3b9155724
Kyle <kj.mckinley@gmail.com>
2021-07-18 14:32:26 -0500

    added express package

--- server.js
+++ server.js
@@ -6,3 +6,5 @@ const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
